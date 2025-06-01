a7481c4c923800ef0748837c2ed770d97292cac3
Arif Rahman <arif.rahman.rhm@gmail.com>
2025-06-01 22:00:55 +0700

    feat  : CORS

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from web.api.v1.routers import router as EbayAPIRouter
 from web.app.routers import router as EbayAppRouter
 
+
 def create_app() -> FastAPI:
     app: FastAPI = FastAPI()
-    
+
     app.include_router(EbayAPIRouter)
     app.include_router(EbayAppRouter)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Add your frontend URL
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
